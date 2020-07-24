f0538d4de0121f906c3957e8bdce0696fdcfc9bb
Sedrick White <sedrick.white@nutanix.com>
2020-07-24 02:11:57 +0000

    add roles for ansible, updated ssh config and ansible host files, new features - Hyper-V

--- config_auth.sh
+++ config_auth.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh $1 '. /etc/profile; ncli authconfig add-directory directory-type=ACTIVE_DIRECTORY connection-type=LDAP directory-url=ldap://learn.local domain=learn.local name=Learn-AD service-account-username=ntnx1@learn.local service-account-password=Nutanix/4u!'
-ssh $1 '. /etc/profile; ncli authconfig add-role-mapping name=Learn-AD role=ROLE_USER_ADMIN entity-type=group entity-values=ntnx_grp'
+ssh $1 '. /etc/profile; ncli authconfig add-directory directory-type=ACTIVE_DIRECTORY connection-type=LDAP directory-url=ldap://dur.sre-labs.nutanix.com domain=dur.sre-labs.nutanix.com name=Dur service-account-username=ntnx1@dur.sre-labs.nutanix.com service-account-password=SREs4eva!'
+ssh $1 '. /etc/profile; ncli authconfig add-role-mapping name=Dur role=ROLE_USER_ADMIN entity-type=group entity-values=ntnx_grp'
