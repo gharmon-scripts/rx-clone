400337c5b9e7a2936cbcef19bf2cc6bf295fc789
Sedrick White <sedrick.white@sedrick-white.ubvm.nutanix.com>
2021-03-04 14:44:55 +0000

    Weekly update

--- deploy_key.sh
+++ deploy_key.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scp ~/.ssh/id_rsa.pub $1:/tmp
-ssh $1 '. /etc/profile; ncli cluster add-public-key name=SW-LNX-MGMT file-path=/tmp/id_rsa.pub'
+ssh $1 '. /etc/profile; ncli cluster add-public-key name=sedrick-white file-path=/tmp/id_rsa.pub'
